d9d9c2a1d929b7607c691bca411764ffc4e13bfa
zhoubenjia <48430095+zhoubenjia@users.noreply.github.com>
2021-03-13 12:07:13 +0800

    Update base.py

--- Network_Train/lib/datasets/base.py
+++ Network_Train/lib/datasets/base.py
@@ -57,21 +57,15 @@ class Datasets(Dataset):
 
             is_flip = False
         return (left, top, left + crop_size, top + crop_size), is_flip
-        # 定义旋转rotate函数
 
     def rotate(self, image, angle, center=None, scale=1.0):
-        # 获取图像尺寸
         (h, w) = image.shape[:2]
-
-        # 若未指定旋转中心，则将图像中心设为旋转中心
         if center is None:
             center = (w / 2, h / 2)
 
-        # 执行旋转
         M = cv2.getRotationMatrix2D(center, angle, scale)
         rotated = cv2.warpAffine(image, M, (w, h))
 
-        # 返回旋转后的图像
         return rotated
 
     def image_propose(self, data_path, sl):
